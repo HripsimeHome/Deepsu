10dea5729101a73277acdb876dfb6f95c3d02b2e
Hripsime <m_hripsime@mail.ru>
2025-02-06 14:34:17 +0400

    Added Impact block

--- src/assets/images.js
+++ src/assets/images.js
@@ -7,7 +7,20 @@ import instantActionImage from "./images/home/instantAction.png";
 import instantActionWebpImage from "./images/home/instantAction.webp";
 import logoWhiteImage from "./images/logos/logoWhite.png"
 
+// Home
 
+import empoweringEveryoneImage from "./images/home/empoweringEveryone.png"
+import empoweringEveryoneWebpImage from "./images/home/empoweringEveryone.webp"
+
+import boostingSecurityImage from "./images/home/boostingSecurity.png" 
+import boostingSecurityWebpImage from "./images/home/boostingSecurity.webp"
+
+import drivingEfficiencyImage from "./images/home/drivingEfficiency.png" 
+import drivingEfficiencyWebpImage from "./images/home/drivingEfficiency.webp"
+
+import enhancingTransparencyImage from "./images/home/enhancingTransparency.png" 
+import enhancingTransparencyWebpImage from "./images/home/enhancingTransparency.webp"
+ 
 export {
 
   // Header
@@ -18,6 +31,20 @@ export {
   // Home
   instantActionImage,  
   instantActionWebpImage,
+
+  empoweringEveryoneImage, 
+  empoweringEveryoneWebpImage,
+
+  boostingSecurityImage,
+  boostingSecurityWebpImage,
+
+  drivingEfficiencyImage,
+  drivingEfficiencyWebpImage,
+
+  enhancingTransparencyImage,
+  enhancingTransparencyWebpImage,
+
+
 
   //Footer
   logoWhiteImage,
