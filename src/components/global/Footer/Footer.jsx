6e1737108b6b14e1cd90abd63445094cc2b47ce1
Hripsime <m_hripsime@mail.ru>
2025-02-07 19:31:23 +0400

    Corrected footer links

--- src/components/global/Footer/Footer.jsx
+++ src/components/global/Footer/Footer.jsx
@@ -18,82 +18,70 @@ import {
 const Footer = () => {
   return (
     <footer className={styles.footer}> 
-      <div className={`${styles.footer__container} container`}>
+      <div className={`${styles.footer__container} container`}>    
+       
         <div>
           <img
-            src={logoWhiteImage}
-            alt="Logo"/>
-          <p className={styles.footer__copyright}>
-            &copy; 2025 Deepsu.ai. All rights reserved.
-          </p>
-        </div>  
-
-
-
-          
-        <div className={styles.footer__menu}>
-        <h3 className={styles.footer__menuTitle}>Resources</h3>
-          <ul className={styles.footer__menuList}>
-            <li>Documentation</li>
-            <li>Terms of use</li>
-            <li>Disclaimer</li>
-          </ul>
+              src={logoWhiteImage}
+              alt="Logo"/>
+            <p className={styles.footer__copyright}>
+              &copy; 2025 Deepsu.ai. All rights reserved.
+            </p>
         </div>
 
-        <div className={styles.footer__menu}>
-        <ul className={styles.footer__menuList}>
-          <h3 className={styles.footer__menuTitle}>Social Media</h3>
-          <a
-              target="_blank"
-              rel="noreferrer"
-              href={twitterUrl}            
-            > 
-            <li>Twitter</li>                              
-            </a>                      
+        <div className={styles.footer__menuContainer}> 
+
+
+          <div>
+          <h3 className={styles.footer__menuTitle}>Resources</h3>
+            <ul className={styles.footer__menuList}>
+              <li>Documentation</li>
+              <li>Terms of use</li>
+              <li>Disclaimer</li>
+            </ul>          
+          </div>
+
+          <div>
+            <h3 className={styles.footer__menuTitle}>Social Media</h3>
+            <ul className={styles.footer__menuList}>           
+              <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={twitterUrl}            
+                > 
+                  <li>Twitter</li>                              
+              </a>               
                   
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={telegramUrl}            
-            > 
-            <li>Telegram</li>                               
-            </a>
-          </ul>
-        </div>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={telegramUrl}            
+              > 
+                <li>Telegram</li>                               
+              </a>
+            </ul>
+          </div>
 
-        <div className={styles.footer__menu}>
-        <ul className={styles.footer__menuList}>
+          <div>
             <h3 className={styles.footer__menuTitle}>Contact</h3>
-            
-            <a
-            target="_blank"
-            rel="noreferrer"
-            href={`mailto:${mailAddress}`}
-            className={styles.footer__contactLink}
-           >
-            <li>info@deepsu.ai</li>
-           </a>
-
-            
-                  
-          </ul>
-        </div>
+            <ul className={styles.footer__menuList}>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={`mailto:${mailAddress}`}
+                className={styles.footer__contactLink}
+            >
+              <li>info@deepsu.ai</li>
+           </a>     
+         </ul>
+        </div>    
 
 
 
-        
-
+        </div> {/* /menuContainer*/}
       </div>  
     </footer>
   );
 };
 
-export default Footer;
-
-        
-      /* <ImageWebp
-            src={instantActionImage}
-            srcSet={instantActionWebpImage}
-            alt="Instant Action"
-            className={styles.homeMain__testImg}
-            />  */
+export default Footer;   
