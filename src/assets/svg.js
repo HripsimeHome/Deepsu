88be16ce29c4f046b01340234835c0fdc2970d5d
Hripsime <m_hripsime@mail.ru>
2025-02-07 14:49:38 +0400

    Added social icons sprite

--- src/assets/svg.js
+++ src/assets/svg.js
@@ -1,14 +1,5 @@
-export const arrowPrevIcon = "arrowPrev";
 
-// Footer
-export const futuresIcon = "futures";
-export const positionIcon = "position";
-export const crypto500Icon = "crypto500";
-export const usdtIcon = "usdt";
-export const moneyIcon = "money";
-export const stackbitIcon = "stackbit"
-
-
-// 1 - home=futures , 2 -profile=usdt, 3 - crypto=crypto, 4 -orders=position
-
-// 1 - home=futures , 2= profile=position, 3 - crypto=crypto, 4 -profile=usdt, 
+// Header
+export const docIcon = "doc"; 
+export const telegramIcon = "telegram";  
+export const twitterIcon = "twitter"; 
--- src/components/global/Header/Header.jsx
+++ src/components/global/Header/Header.jsx
@@ -1,5 +1,6 @@
 import styles from "./Header.module.scss";
-//import Svg from "../../layout/Svg/Svg";
+import { Link } from "react-router-dom";
+import Svg from "../../layout/Svg/Svg"
 
 import {
   logoTextImage,  
@@ -8,7 +9,10 @@ import {
 } from "../../../assets/images";
 
 import {
-   //arrowPrevIcon 
+   docIcon,
+   twitterIcon,
+   arrowPrevIcon,
+   telegramIcon
 } from "../../../assets/svg"; 
 
 const Header = () => {      
@@ -19,24 +23,46 @@ const Header = () => {
         <div className={styles.header__container}>
 
 
-        <div className={styles.header__infoBLock}>
+          <div className={styles.header__infoBLock}>
 
-          <div>
-            <h1 className={styles.header__title}>Next
-             {/* <img src={suiImage} alt="SUI"/>*/}
-             Gen  <span className="texGreen">AI</span>
-            </h1>             
-          </div>
+            <div>
+              <h1 className={styles.header__title}>Next
+              {/* <img src={suiImage} alt="SUI"/>*/}
+              Gen  <span className="texGreen">AI</span>
+              </h1>             
+            </div>
           
-          <div className={styles.header__infoText}>
-          DeFi token management platform on Sui blockchain. Use AI and data analytics to simplify crypto operations, optimize portfolios, and secure assets.        
-          
-          </div>
+            <div className={styles.header__infoText}>
+            DeFi token management platform on Sui blockchain. Use AI and data analytics to simplify crypto operations, optimize portfolios, and secure assets.        
+            
+            </div>
 
-          <div className={styles.header__btnPosition}>
-          <button className={styles.header__btn}>Launch App</button>
-          Documentaiton
-          </div>
+            <div className={styles.header__btnPosition}>
+            <button className={styles.header__btn}>Launch App</button>
+
+           
+
+           
+            <div className={styles.header__docPosition}>
+              <Svg 
+                id={docIcon} 
+                className={styles.header__docIcon}/>
+                <span className={styles.header__docText}>Documentation</span>
+
+                  </div>
+
+            </div>
+
+<div className={styles.header__socIconsPosition}>
+            <Svg
+             id={telegramIcon} 
+             className={styles.header__socIcon}/>
+
+            <Svg 
+              id={twitterIcon}
+              className={styles.header__socIcon}/>
+
+</div>
 
         </div> {/* /infoBLock}*/}
 
