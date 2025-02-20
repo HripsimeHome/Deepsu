e09a16dae9224eee763612e1e6c05cc445a40d42
Hripsime <m_hripsime@mail.ru>
2025-02-20 15:58:56 +0400

    Added close icon in mobile menu

--- src/components/global/HeaderMenu/HeaderMenu.jsx
+++ src/components/global/HeaderMenu/HeaderMenu.jsx
@@ -1,118 +1,80 @@
-import styles from "./HeaderMenu.module.scss";
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { headerMenuItems } from "../../../constants/menuItems"
-import Svg from "../../layout/Svg/Svg";
+import styles from "./HeaderMenu.module.scss";
+import { headerMenuItems } from "../../../constants/menuItems";
 import { twitterUrl, telegramUrl } from "../../../constants/contacts";
+import { twitterIcon, telegramIcon, burgerIcon, crossIcon } from "../../../assets/svg";
+import { logoGreenImage } from "../../../assets/images";
+import { chatPagePath, homePagePath } from "../../../router/path";
+import Svg from "../../layout/Svg/Svg";
 
-import {
-  logoGreenImage
-} from "../../../assets/images";
+const HeaderMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-import { 
-  twitterIcon, 
-  telegramIcon, 
-  burgerIcon, 
-  crossIcon
-} from "../../../assets/svg";
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-import {
-  chatPagePath,
-  homePagePath 
-} from "../../../router/path";
- 
-const HeaderMenu = () => {  
-const [isMenuOpen, setIsMenuOpen] = useState(false);
- 
- const toggleMenu = () => {
-  setIsMenuOpen(!isMenuOpen);
- };
-
-  return (            
+  return (
     <div className={styles.headerMenu}>
       <div className={`${styles.headerMenu__container} container`}>
-        <div>
-          <div className={styles.headerMenu__leftBlock}>
-            <Link to={homePagePath}>
-              <img
-                src={logoGreenImage} 
-                alt="DeepsuAI"
-                className={styles.headerMenu__logoImg}
-              />
-            </Link>
-             
-             <nav className={`${styles.headerMenu__menu} ${isMenuOpen ? styles.headerMenu__menu_open : ""}`}>         
-              {headerMenuItems.map(({ text, link }, index) => (  
-              <NavLink 
+        {/* Left Block */}
+        <div className={styles.headerMenu__leftBlock}>
+          <Link to={homePagePath}>
+            <img src={logoGreenImage} alt="DeepsuAI" className={styles.headerMenu__logoImg} />
+          </Link>
+
+          {/* Burger Icon to Open Menu */}
+          <button className={styles.headerMenu__burger} onClick={toggleMenu}>
+            <Svg id={burgerIcon} className={styles.headerMenu__icon} />
+          </button>
+        </div>
+
+        {/* Right Block */}
+        <div className={styles.headerMenu__rightBlock}>
+          <a href={telegramUrl} target="_blank" rel="noreferrer">
+            <Svg id={telegramIcon} className={styles.headerMenu__socIcon} />
+          </a>
+          <a href={twitterUrl} target="_blank" rel="noreferrer">
+            <Svg id={twitterIcon} className={styles.headerMenu__socIcon} />
+          </a>
+
+          <Link to={chatPagePath} className={styles.headerMenu__btnConnect}>
+            Connect Wallet
+          </Link>
+        </div>
+      </div>
+
+      {/* Navigation Menu Modal */}
+      {isMenuOpen && (
+        <>
+          <nav className={styles.headerMenu__menu}>
+            {/* Close Button Inside Modal */}
+            <button className={styles.headerMenu__close} onClick={toggleMenu}>
+              <Svg id={crossIcon} className={styles.headerMenu__closeIcon} />
+            </button>
+
+            {headerMenuItems.map(({ text, link }, index) => (
+              <NavLink
                 to={link}
                 key={index}
                 className={({ isActive }) =>
-                 isActive
-                      ? `${styles.headerMenu__menuLink} ${styles.headerMenu__menuLink_active}`
-                      : styles.headerMenu__menuLink
-                  }
-                  onClick={() => setIsMenuOpen(false)}
-                >
+                  isActive
+                    ? `${styles.headerMenu__menuLink} ${styles.headerMenu__menuLink_active}`
+                    : styles.headerMenu__menuLink
+                }
+                onClick={() => setIsMenuOpen(false)}
+              >
                 {text}
-              </NavLink> 
-             ))}
-            </nav>
-          </div>
-        </div>
-         
+              </NavLink>
+            ))}
+          </nav>
 
-
-         
-        <div> 
-          <div className={styles.headerMenu__rightblock}>
-            <a 
-              href={telegramUrl}
-              target="_blank" 
-              rel="noreferrer"
-            >
-              <Svg 
-              id={telegramIcon} 
-              className={styles.headerMenu__socIcon} 
-              />
-            </a>
-            <a 
-              href={twitterUrl}
-              target="_blank" 
-              rel="noreferrer"           
-            >
-              <Svg 
-                id={twitterIcon} 
-                className={styles.headerMenu__socIcon} 
-              />
-            </a>    
-
-
-
-          <button 
-          onClick={toggleMenu}
-          >
-            <Svg 
-              id={isMenuOpen ? crossIcon : burgerIcon}               
-              className={styles.headerMenu__burgerIcon}
-              />
-          </button>
-
-
-
-
-
-            <Link
-             to={chatPagePath}
-             className={styles.headerMenu__btnConnect}>
-            Connect Wallet
-            </Link>
-          </div>        
-        </div> {/* Right block */ }
-      </div>  
-      {isMenuOpen && <div className={styles.headerMenu__overlay} onClick={toggleMenu}></div>}
-
-         
-    </div>    
+          {/* Overlay to Close Menu When Clicking Outside */}
+          <div className={styles.headerMenu__overlay} onClick={toggleMenu}></div>
+        </>
+      )}
+    </div>
   );
 };
 
